6cbebb77fda53097ef3dabe456cde36f30dde697
ferdiansyah6706154192 <edosetiawan108@gmail.com>
2017-05-04 20:41:38 +0700

    Add files via upload

---
+++ javaneseculture/admin.php
@@ -0,0 +1,85 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="Description" content="Mengenalkan ragam budaya tari tradisional sumatera selatan">
+	<meta name="Author" content="Musik II">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/owl.theme.css">
+		<link rel="stylesheet" href="css/owl.transitions.css">
+		<link rel="icon" href="img/header.jpg" type="image/x-icon">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+	<link href='#' rel='stylesheet' type='text/css'>
+	<title>Javanese Culture Heritage</title>
+</head>
+<body>
+	<div class="navbar">
+	<img src="img/header.png">
+	<ul>
+	<li> <a href="admin.php"> <i class="fa fa-user"> Admin </i> </a> </li>
+	<li> <a href="aboutus.php"><i class="fa fa-phone">   About Us  </i> </a> </li>
+	<li> <a href="#"><i class="fa fa-child">  Provinsi </i> </a>  
+	<ul>
+	<li> <a href="tari-gending-sriwijaya.php"> Propinsi Jawa Barat </a> </li>
+	<li> <a href="tari-tanggai.php"> Propinsi Jawa Timur </a> </li>
+	<li> <a href="tari-mejeng-basuko.php"> Propinsi Jawa Tengah </a> </li>
+	</ul>
+	</li>
+	<li> <a href="index.php"> <i class="fa fa-home"> Home  </i> </a> </li>
+	</ul>
+	</div>
+	<div class="content-utama">
+	 <h4> <b> Upload Data </b> </h4> 
+	<p>
+	
+	 </p>
+
+</p> <br>
+
+
+
+</p>
+	</div>
+	<div class="content">
+	
+
+	<div class="content content-1">
+	<a href="tari-gending-sriwijaya.html"> <img src="img/1.jpg"> </a>
+
+	</div>
+	<div class="content content-2">
+	<a href="tari-tanggai.html"> <img src="img/2.jpg"> </a>
+	</div>
+	<div class="content content-3">
+	<a href="tari-mejeng-basuko.html"> <img src="img/3.jpg"> </a>
+	</div>
+	<div class="content content-4">
+	<a href="tari-rodat-cempako.html"> <img src="img/4.jpg"> </a>
+	</div>
+	<div class="content content-5">
+	<a href="tari-tenun-songket.html"> <img src="img/5.jpg"> </a>
+	</div>
+	<div class="content content-6">
+	<a href="tari-pagar-pengantin.html"> <img src="img/6.jpg"> </a>
+	</div>
+	</div>
+
+
+	<div class="footer">
+	<b> Copyright &copy; 2016 - Kwbudayaan Jawa By Musik II  </b>
+	<a href="#">
+	<i class="fa fa-facebook"> </i>
+	<i class="fa fa-twitter"> </i>
+	<i class="fa fa-instagram"> </i>
+	<i class="fa fa-google-plus"> </i>
+	
+	
+	
+	</a>
+	
+	</div>
+
+
+
+</body>
+</html>
