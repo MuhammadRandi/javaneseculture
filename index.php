bb3771b9080655e78dc6e564eef51f8cfdb89c44
Hildan Fawwaz Naufal <hildanfn01@gmail.com>
2017-05-05 20:59:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="Description" content="Mengenalkan ragam budaya tari tradisional sumatera selatan">
+	<meta name="Author" content="Musik II">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/owl.theme.css">
+		<link rel="stylesheet" href="css/owl.transitions.css">
+		<link rel="icon" href="img/header.jpg" type="image/x-icon">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+	<link href='#' rel='stylesheet' type='text/css'>
+	<title>Javanese Culture Heritage</title>
+</head>
+<body>
+	<div class="navbar">
+	<img src="img/header.png">
+	<ul>
+<li> <a href="admin.php"> <i class="fa fa-user"> Admin </i> </a> </li>
+	<li> <a href="aboutus.php"><i class="fa fa-phone">   About Us  </i> </a> </li>
+	<li> <a href="#"><i class="fa fa-child">  Provinsi </i> </a>  
+	<ul>
+	<li> <a href="Provinsijawabarat.php"> Propinsi Jawa Barat </a> </li>
+	<li> <a href="Provinsijawatimur.php"> Propinsi Jawa Timur </a> </li>
+	<li> <a href="Provinsijawatengah.php"> Propinsi Jawa Tengah </a> </li>
+	</ul>
+	</li>
+	<li> <a href="index.php"> <i class="fa fa-home"> Home  </i> </a> </li>
+	</ul>
+	</div>
+	<div id="slideshow" class="owl-carousel owl-theme">
+				<div class="item"><img src="img/imgg/angklung.jpg">
+
+					
+				</div>
+				<div class="item"><img src="img/imgg/badutmalang.jpg">
+					
+				</div>	
+				<div class="item"><img src="img/imgg/budha.jpg">
+					
+				</div>	
+				<div class="item"><img src="img/imgg/dukuhsalatiga.jpg">
+					
+				</div>	
+				<div class="item"><img src="img/imgg/gamelan.jpg">
+					
+				</div>	
+				<div class="item"><img src="img/imgg/jenangkudus.jpg">
+					
+				</div>				
+			</div>
+	<div class="content">
+	<h4> Javanese Culture Heritage </h4>
+	<hr width="530px" style="margin-left:420px;">
+	<div class="content content-1">
+	<img src="img/imgg/budha.jpg"> </a>
+	</div>
+	<div class="content content-2">
+	<img src="img/imgg/angklung.jpg"> </a>
+	</div>
+	<div class="content content-3">
+	<img src="img/imgg/badutmalang.jpg">
+	</div>
+	<div class="content content-4">
+	<img src="img/imgg/brahu.jpg"> </a>
+	</div>
+	<div class="content content-5">
+	<img src="img/imgg/dukuhsalatiga.jpg"> </a>
+	</div>
+	<div class="content content-6">
+	<img src="img/imgg/gamelan.jpg"> </a>
+	</div>
+	</div>
+
+	<div class="footer">
+	<b> Copyright &copy; 2017 - Kebudayaan Jawa  </b>
+	<a href="#">
+	<i class="fa fa-facebook"> </i>
+	<i class="fa fa-twitter"> </i>
+	<i class="fa fa-instagram"> </i>
+	<i class="fa fa-google-plus"> </i>
+	
+	
+	
+	</a>
+	
+	
+	</div>
+
+
+
+
+
+
+			<script src="js/jquery-1.11.3.js"></script>
+			<script src="js/owl.carousel.js"></script>
+			<script>
+				
+						$(document).ready(function() {
+						$("#slideshow").owlCarousel({
+						  navigation : true, // Show next and prev buttons
+						  navigationText : ["<",">"],
+						  slideSpeed : 300,
+						  paginationSpeed : 100,
+						  singleItem:true,
+						  autoPlay:5000, //untuk gerakin slide
+						  transitionStyle:"backSlide",
+						  mouseDrag:false
+					  
+				 
+					  // "singleItem:true" is a shortcut for:
+					  // items : 1, 
+					  // itemsDesktop : false,
+					  // itemsDesktopSmall : false,
+					  // itemsTablet: false,
+					  // itemsMobile : false
+					  
+				 
+				  });
+				 
+				});
+			</script>
+	
+</body>
+</html>
